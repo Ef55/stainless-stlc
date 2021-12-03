e6145b1cb7057c346707917fd17435e444dce0e5
Noe De Santo <noe.desanto@epfl.ch>
2021-12-03 14:48:06 +0100

    Implement `tabsSubstitution`

--- src/Reduction.scala
+++ src/Reduction.scala
@@ -88,7 +88,7 @@ object Reduction {
     }
   }
 
-    // ↑⁻¹( [0 -> ↑¹(arg)]body )
+  // ↑⁻¹( [0 -> ↑¹(arg)]body )
   def absSubstitution(body: Type, arg: Type): Type = {
     assert(!arg.hasFreeVariablesIn(0, 0))
     boundRangeShift(arg, 1, 0, 0)
@@ -97,94 +97,167 @@ object Reduction {
     shift(substitute(body, 0, shift(arg, 1, 0)), -1, 0)
   }
 
+  def negativeTypeShiftValidity(t: Term, d: BigInt, c: BigInt): Boolean = {
+    require(d < 0)
+    t match {
+      case Var(k) => true
+      case Abs(typ, body) => negativeShiftValidity(typ, d, c) && negativeTypeShiftValidity(body, d, c)
+      case App(t1, t2) => negativeTypeShiftValidity(t1, d, c) && negativeTypeShiftValidity(t2, d, c)
+      case Fix(f) => negativeTypeShiftValidity(f, d, c)
+      case TAbs(body) => negativeTypeShiftValidity(body, d, c+1)
+      case TApp(t, typ) => negativeTypeShiftValidity(t, d, c) && negativeShiftValidity(typ, d, c)
+    }
+  }
+
+  def typesShift(t: Term, d: BigInt, c: BigInt): Term = {
+    require(d >= 0 || negativeTypeShiftValidity(t, d, c))
+    require(c >= 0)
+    t match {
+      case Var(k) => t
+      case Abs(typ, body) => Abs(shift(typ, d, c), typesShift(body, d, c))
+      case App(t1, t2) => App(typesShift(t1, d, c), typesShift(t2, d, c))
+      case Fix(f) => Fix(typesShift(f, d, c))
+      case TAbs(body) => TAbs(typesShift(body, d, c+1))
+      case TApp(t, typ) => TApp(typesShift(t, d, c), shift(typ, d, c))
+    }
+  }
+
+  def substitute(t: Term, v: BigInt, s: Type): Term = {
+    t match {
+      case Var(k) => t
+      case Abs(typ, body) => Abs(substitute(typ, v, s), substitute(body, v, s))
+      case App(t1, t2) => App(substitute(t1, v, s), substitute(t2, v, s))
+      case Fix(f) => Fix(substitute(f, v, s))
+      case TAbs(body) => TAbs(substitute(body, v+1, s))
+      case TApp(t, typ) => TApp(substitute(t, v, s), substitute(typ, v, s))
+    }
+  }
+
+  def tabsSubstitution(body: Term, arg: Type): Term = {
+    assert(!arg.hasFreeVariablesIn(0, 0))
+    boundRangeShift(arg, 1, 0, 0)
+    boundTypeRangeSubstitutionLemma(body, 0, shift(arg, 1, 0))
+    boundTypeRangeShiftBackLemma(substitute(body, 0, shift(arg, 1, 0)), 1, 0)
+    typesShift(substitute(body, 0, shift(arg, 1, 0)), -1, 0)
+  }
+
   /// General lambda calculus evaluation
 
-  // // [t -> t']
-  // def reducesTo(t: Term, tp: Term): Boolean = {
-  //   t match {
-  //     case Var(_) => false
-  //     case Abs(_, _) => false
-  //     case App(t1, t2) => {
-  //       (tp match {
-  //         case App(t1p, t2p) if reducesTo(t1, t1p) && t2 == t2p  => true
-  //         case App(t1p, t2p) if t1 == t1p && reducesTo(t2, t2p) => true
-  //         case _ => false
-  //       }) || (t1 match {
-  //         case Abs(_, body) => absSubsitution(body, t2) == tp
-  //         case _ => false
-  //       })
-  //     }
-  //     case Fix(f) => {
-  //       (tp match {
-  //         case Fix(fp) => reducesTo(f, fp)
-  //         case _ => false
-  //       }) || (f match {
-  //         case Abs(_, body) => absSubsitution(body, t) == tp
-  //         case _ => false
-  //       })
-  //     }
-  //   }
-  // }
+  // [t -> t']
+  def reducesTo(t: Term, tp: Term): Boolean = {
+    t match {
+      case Var(_) => false
+      case Abs(_, _) => false
+      case App(t1, t2) => {
+        (tp match {
+          case App(t1p, t2p) if reducesTo(t1, t1p) && t2 == t2p  => true
+          case App(t1p, t2p) if t1 == t1p && reducesTo(t2, t2p) => true
+          case _ => false
+        }) || (t1 match {
+          case Abs(_, body) => absSubsitution(body, t2) == tp
+          case _ => false
+        })
+      }
+      case Fix(f) => {
+        (tp match {
+          case Fix(fp) => reducesTo(f, fp)
+          case _ => false
+        }) || (f match {
+          case Abs(_, body) => absSubsitution(body, t) == tp
+          case _ => false
+        })
+      }
+      case TAbs(body) => false
+      case TApp(term, typ) => {
+        (tp match {
+          case TApp(termp, typp) => reducesTo(term, termp) && typ == typp
+          case _ => false
+        }) || (term match {
+          case TAbs(body) => tabsSubstitution(body, typ) == tp
+          case _ => false
+        })
+      }
+    }
+  }
 
-  // // { t' | t -> t' }
-  // def reduceAll(t: Term): Set[Term] = {
-  //   t match {
-  //     case Var(_) => Set[Term]()
-  //     case Abs(_, _) => Set[Term]()
-  //     case App(t1, t2) => {
-  //       reduceAll(t1).map[Term](t1p => App(t1p, t2)) ++ 
-  //         reduceAll(t2).map[Term](t2p => App(t1, t2p)) ++ 
-  //         (t1 match {
-  //           case Abs(_, body) => Set[Term](absSubsitution(body, t2))
-  //           case _ => Set[Term]()
-  //         })
-  //     }
-  //     case Fix(f) => {
-  //       f match {
-  //         case Abs(_, body) => reduceAll(f).map[Term](Fix(_)) + absSubsitution(body, t)
-  //         case _ => reduceAll(f).map(Fix(_))
-  //       }
-        
-  //     }
-  //   }
-  // }
+  // { t' | t -> t' }
+  def reduceAll(t: Term): Set[Term] = {
+    t match {
+      case Var(_) => Set[Term]()
+      case Abs(_, _) => Set[Term]()
+      case App(t1, t2) => {
+        reduceAll(t1).map[Term](t1p => App(t1p, t2)) ++ 
+          reduceAll(t2).map[Term](t2p => App(t1, t2p)) ++ 
+          (t1 match {
+            case Abs(_, body) => Set[Term](absSubsitution(body, t2))
+            case _ => Set[Term]()
+          })
+      }
+      case Fix(f) => {
+        f match {
+          case Abs(_, body) => reduceAll(f).map[Term](Fix(_)) + absSubsitution(body, t)
+          case _ => reduceAll(f).map(Fix(_))
+        }
+      }
+      case TAbs(body) => Set[Term]()
+      case TApp(term, typ) => {
+        reduceAll(term).map[Term](TApp(_, typ)) ++
+          (term match {
+            case TAbs(body) => Set[Term](tabsSubstitution(body, typ))
+            case _ => Set[Term]()
+          })
+      }
+    }
+  }
 
-  // /// Call-by-value lambda calculus evaluation
+  /// Call-by-value lambda calculus evaluation
 
-  // def reduceCallByValue(t: Term): Option[Term] = {
-  //   t match {
-  //     case Var(_) => None[Term]()
-  //     case Abs(_, _) => None[Term]()
-  //     case App(t1, t2) => {
-  //       if(!t1.isValue) {
-  //         reduceCallByValue(t1).map(t1p => App(t1p, t2))
-  //       }
-  //       else if(!t2.isValue) {
-  //         reduceCallByValue(t2).map(t2p => App(t1, t2p))
-  //       }
-  //       else {
-  //         t1 match {
-  //           case Abs(_, body) => {
-  //             Some(absSubsitution(body, t2))
-  //           }
-  //           case _ => None[Term]()
-  //         }
-  //       }
-  //     }
-  //     case Fix(f) => {
-  //       if(!f.isValue) {
-  //         reduceCallByValue(f).map(Fix(_))
-  //       }
-  //       else {
-  //         f match {
-  //           case Abs(_, body) => {
-  //             Some(absSubsitution(body, t))
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  def reduceCallByValue(t: Term): Option[Term] = {
+    t match {
+      case Var(_) => None[Term]()
+      case Abs(_, _) => None[Term]()
+      case App(t1, t2) => {
+        if(!t1.isValue) {
+          reduceCallByValue(t1).map(t1p => App(t1p, t2))
+        }
+        else if(!t2.isValue) {
+          reduceCallByValue(t2).map(t2p => App(t1, t2p))
+        }
+        else {
+          t1 match {
+            case Abs(_, body) => {
+              Some(absSubsitution(body, t2))
+            }
+            case _ => None[Term]()
+          }
+        }
+      }
+      case Fix(f) => {
+        if(!f.isValue) {
+          reduceCallByValue(f).map(Fix(_))
+        }
+        else {
+          f match {
+            case Abs(_, body) => {
+              Some(absSubsitution(body, t))
+            }
+          }
+        }
+      }
+      case TAbs(body) => None[Term]()
+      case TApp(term, typ) => {
+        if(!term.isValue) {
+          reduceCallByValue(term).map[Term](TApp(_, typ))
+        }
+        else {
+          term match {
+            case TAbs(body) => Some(tabsSubstitution(body, typ))
+            case _ => None[Term]()
+          }
+        }
+      }
+    }
+  }
 
 }
 
@@ -349,6 +422,7 @@ object ReductionProperties {
 
   /// Substitution & shifting lemmas on types
 
+  @opaque @pure
   def boundRangeShiftComposition(t: Type, a: BigInt, b: BigInt, c: BigInt, d: BigInt): Unit = {
     require(a >= 0)
     require(c >= 0)
@@ -391,6 +465,7 @@ object ReductionProperties {
     }
   }.ensuring(shift(shift(t, a, c), b, d) == shift(t, a + b, c))
 
+  @opaque @pure
   def boundRangeShift(t: Type, d: BigInt, c: BigInt, b: BigInt): Unit = {
     require(c >= 0)
     require(d >= 0)
@@ -413,6 +488,7 @@ object ReductionProperties {
 
   }.ensuring(!shift(t, d, c).hasFreeVariablesIn(c, d+b))
 
+  @opaque @pure
   def boundRangeShiftBelowCutoff(t: Type, d: BigInt, c: BigInt, a: BigInt, b: BigInt): Unit = {
     require(d >= 0)
     require(c >= 0)
@@ -432,6 +508,7 @@ object ReductionProperties {
     }
   }.ensuring(!shift(t, d, c).hasFreeVariablesIn(a, b))
 
+  @opaque @pure
   def boundRangeSubstitutionLemma(t: Type, j: BigInt, s: Type): Unit = {
     require(j >= 0)
     require(!s.hasFreeVariablesIn(0, j+1))
@@ -453,6 +530,7 @@ object ReductionProperties {
     }
   }.ensuring(!substitute(t, j, s).hasFreeVariable(j))
 
+  @opaque @pure
   def boundRangeShiftBackLemma(t: Type, d: BigInt, c: BigInt): Unit = {
     require(c >= 0)
     require(d > 0)
@@ -472,6 +550,61 @@ object ReductionProperties {
       }
     }
   }.ensuring(negativeShiftValidity(t, -d, c))
+
+  /// Substitution & shifting lemmas on types in terms
+
+  @opaque @pure
+  def boundTypeRangeSubstitutionLemma(t: Term, j: BigInt, s: Type): Unit = {
+    require(j >= 0)
+    require(!s.hasFreeVariablesIn(0, j+1))
+
+    t match {
+      case Var(k) => assert(!substitute(t, j, s).hasFreeVariable(j))
+      case Abs(targ, body) => {
+        boundRangeSubstitutionLemma(targ, j, s)
+        boundTypeRangeSubstitutionLemma(body, j, s)
+        assert(!substitute(t, j, s).hasFreeVariable(j))
+      }
+      case App(t1, t2) => {
+        boundTypeRangeSubstitutionLemma(t1, j, s)
+        boundTypeRangeSubstitutionLemma(t2, j, s)
+        assert(!substitute(t, j, s).hasFreeVariable(j))
+      }
+      case Fix(f) => boundTypeRangeSubstitutionLemma(f, j, s)
+      case TAbs(body) => boundTypeRangeSubstitutionLemma(body, j+1, s)
+      case TApp(term, typ) => {
+        boundTypeRangeSubstitutionLemma(term, j, s)
+        boundRangeSubstitutionLemma(typ, j, s)
+      }
+    }
+  }.ensuring(!substitute(t, j, s).hasFreeTypeVariable(j))
+
+  @opaque @pure
+  def boundTypeRangeShiftBackLemma(t: Term, d: BigInt, c: BigInt): Unit = {
+    require(c >= 0)
+    require(d > 0)
+    require(!t.hasFreeTypeVariablesIn(c, d))
+
+    t match {
+      case Var(k) => assert(negativeTypeShiftValidity(t, -d, c))
+      case Abs(targ, body) => {
+        boundRangeShiftBackLemma(targ, d, c)
+        boundTypeRangeShiftBackLemma(body, d, c)
+        assert(negativeTypeShiftValidity(t, -d, c))
+      }
+      case App(t1, t2) => {
+        boundTypeRangeShiftBackLemma(t1, d, c)
+        boundTypeRangeShiftBackLemma(t2, d, c)
+        assert(negativeTypeShiftValidity(t, -d, c))
+      }
+      case Fix(f) => boundTypeRangeShiftBackLemma(f, d, c)
+      case TAbs(body) => boundTypeRangeShiftBackLemma(body, d, c+1)
+      case TApp(term, typ) => {
+        boundTypeRangeShiftBackLemma(term, d, c)
+        boundRangeShiftBackLemma(typ, d, c)
+      }
+    }
+  }.ensuring(negativeTypeShiftValidity(t, -d, c))
 
   /// ReduceAll correctness
 
